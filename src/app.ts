9618bf2d94bb54078e5ab593ef07e0c622105d84
Lech Stawikowski <lech.stawikowski@augmentersoft.com>
2019-09-21 10:10:54 +0200

    basic styles, generating dummy content

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,39 @@
-import Msg from './Message'
+import Msg from './Message';
+import { LoremIpsum } from "lorem-ipsum";
 
 window.onload = function(){
+
+    const dummyContentGenerator = new LoremIpsum({
+        sentencesPerParagraph: {
+            max: 8,
+            min: 4
+        },
+        wordsPerSentence: {
+            max: 16,
+            min: 4
+        }
+    });
+
     let o = new Msg();
     o.show();
+
+    let mainContainer = document.getElementById("container");
+    const maxHeight = mainContainer.clientHeight;
+
+    console.log(maxHeight);
+
+    let innerContainer = document.getElementById("content");
+
+    let bool = false;
+
+    do {
+        if ( innerContainer.clientHeight < maxHeight ) {
+            bool = true;
+            innerContainer.innerHTML += "<p>"+dummyContentGenerator.generateParagraphs(1)+"</p>";
+            console.log(innerContainer.clientHeight);
+        } else {
+            bool = false;
+        }
+    } while (bool);
+
 };
